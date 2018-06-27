4972d08a388782a9cdc00fefdc897cb2a595ca0c
aodom3 <aodom3@gmail.com>
2018-06-27 12:19:36 -0400

    completed editing all manga routes for CRUD

--- routes/manga.js
+++ routes/manga.js
@@ -4,18 +4,43 @@ const router = express.Router({mergeParams: true})
 const Manga = require('../db/schema')
  
 /*GET ALL THE MANGAS*/
-router.get('/')
+router.get('/', function (req, res,next){
+    Manga.find(function(err, products) {
+        if (err) return next (err)
+        res.json(produts)
+    })
+})
 
 /*GET SINGLE MANGA*/
-router.get()
+router.get('/:id', function (req, res,next){
+    Manga.findById (req.params.id, function(err, post) {
+        if (err) return next (err)
+        res.json(post)
+    })
+})
  
 /*SAVE MANGA*/
-router.post()
+router.post('/', function (req, res,next){
+    Manga.create(req.body, function(err, products) {
+        if (err) return next (err)
+        res.json(post)
+    })
+})
  
 /*UPDATE MANGA*/
-router.put()
+router.put('/:id', function (req, res, next){
+    Manga.findByIdAndUpdate (req.params.id, req.body, function (err, post){
+        if (err) return next (err);
+        res.json(post)
+    })
+})
 
  /*DELETE MANGA*/
-router.delete()
+router.delete('/:id', function (req, res, next){
+    Manga.findByIdAndRemove(req.params.id, req.body, function (err, post){
+        if (err) return next (err);
+        res.json(post)
+    })
+})
 
 module.exports = router;
