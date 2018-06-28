52cf498ad64da8df5ff5f04deea1023622ae73c3
aodom3 <aodom3@gmail.com>
2018-06-28 17:46:22 -0400

    seeds are saved and showing up in terminal

--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-mongoose.Promise = global.promise
 mongoose.connect(process.env.MONGODB_URI)
+// mongoose.Promise = global.promise
+
 
 
 const {User, Pic} = require('./schema')
@@ -14,15 +15,15 @@ const Month2 = new Pic({
     title: 'Second Month',
     description: 'You are growing so quickly'
 })
-const Ashley = new User{(
+const Ashley = new User({
     userName:'AshLee',
     password: '1234',
-)}
+})
 
 User.remove({})
     .then(() => Ashley.save())
     .then(() => console.log('Saved!'))
     .then(() => mongoose.connection.close())
-    .catch.(console.error)
+    .catch(console.error)
 
 
